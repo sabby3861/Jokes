1b3922a438fc8a438bf3caba244109824971089f
Sanjay Chauhan <sanjay.22.kumar@bt.com>
2021-07-18 19:14:23 +0100

    Implemented activity and alert view helper classes

---
+++ Jokes/Jokes/RJExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  RJExtensions.swift
+//  Jokes
+//
+//  Created by Sanjay Chauhan on 18/07/2021.
+//
+
+import UIKit
+/// A Helper to remove view from the superview
+extension UIView {
+    func remove() {
+        self.removeFromSuperview()
+    }
+}
